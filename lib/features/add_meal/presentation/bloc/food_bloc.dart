e9e87abf65eb217859255b686ec28af1ba6cec4b
elqot_7 <hegab837@gmail.com>
2025-05-01 15:20:13 +0300

    Initial commit

---
+++ lib/features/add_meal/presentation/bloc/food_bloc.dart
@@ -0,0 +1,53 @@
+import 'package:active_fit/core/domain/usecase/get_config_usecase.dart';
+import 'package:active_fit/features/add_meal/domain/entity/meal_entity.dart';
+import 'package:active_fit/features/add_meal/domain/usecase/search_products_usecase.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:logging/logging.dart';
+
+
+part 'food_event.dart';
+
+part 'food_state.dart';
+
+class FoodBloc extends Bloc<FoodEvent, FoodState> {
+  final log = Logger('FoodBloc');
+
+  final SearchProductsUseCase _searchProductUseCase;
+  final GetConfigUsecase _getConfigUsecase;
+
+  String _searchString = "";
+
+  FoodBloc(this._searchProductUseCase, this._getConfigUsecase)
+      : super(FoodInitial()) {
+    on<LoadFoodEvent>((event, emit) async {
+      if (event.searchString != _searchString) {
+        _searchString = event.searchString;
+        emit(FoodLoadingState());
+        try {
+          final result =
+              await _searchProductUseCase.searchFDCFoodByString(_searchString);
+          final config = await _getConfigUsecase.getConfig();
+
+          emit(FoodLoadedState(
+              food: result, usesImperialUnits: config.usesImperialUnits));
+        } catch (error) {
+          log.severe(error);
+          emit(FoodFailedState());
+         print(error.toString());
+        }
+      }
+    });
+    on<RefreshFoodEvent>((event, emit) async {
+      emit(FoodLoadingState());
+      try {
+        final result =
+            await _searchProductUseCase.searchFDCFoodByString(_searchString);
+        emit(FoodLoadedState(food: result));
+      } catch (error) {
+        log.severe(error);
+        emit(FoodFailedState());
+      }
+    });
+  }
+}
